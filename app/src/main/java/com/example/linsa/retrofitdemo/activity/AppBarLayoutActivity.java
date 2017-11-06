a0662859368ffd65c471c85ffc38ab935b3aa8f4
OnexZgj <474835552@qq.com>
2017-11-06 15:35:39 +0800

    Update AppBarLayoutActivity.java

--- app/src/main/java/com/example/linsa/retrofitdemo/activity/AppBarLayoutActivity.java
+++ app/src/main/java/com/example/linsa/retrofitdemo/activity/AppBarLayoutActivity.java
@@ -9,7 +9,7 @@ import com.example.linsa.retrofitdemo.R;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-//测试AppBarLayout的使用
+//测试AppBarLayout的使用 By张更杰
 public class AppBarLayoutActivity extends AppCompatActivity {
 
     @InjectView(R.id.tb_aabl_toolbar)
