b4bfb2093a8209f09c6c85f8b65284e74c4c3fda
OnexZgj <474835552@qq.com>
2017-12-14 10:19:35 +0800

    add  canvas  api  test and ViewAnimationUtils

---
+++ app/src/main/java/com/example/linsa/retrofitdemo/activity/ViewAnimationUtilsActivity.java
@@ -0,0 +1,15 @@
+package com.example.linsa.retrofitdemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.linsa.retrofitdemo.R;
+
+public class ViewAnimationUtilsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_animation_utils);
+    }
+}
